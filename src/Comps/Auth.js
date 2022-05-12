c711bf248e98c8ca5b8d434cf008e5381e80d58f
Geahn <contato@geahn.com>
2022-05-12 00:27:16 -0300

    vai

--- src/Comps/Auth.js
+++ src/Comps/Auth.js
@@ -9,7 +9,7 @@ class Auth extends React.Component {
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                console.log(data['data']['username']);
+                console.log(data['data'][0]['username']);
             })
             .catch(error => {
                 console.log('ERROR: ' + error.message);
