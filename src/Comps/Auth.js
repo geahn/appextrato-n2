9962931c1c8fe07240d0ae59dd718683a33a61b7
Geahn <contato@geahn.com>
2022-05-27 20:36:20 -0300

    vai

--- src/Comps/Auth.js
+++ src/Comps/Auth.js
@@ -95,16 +95,22 @@ class Auth extends React.Component {
             //         console.error('ERROR: ' + error.message);
             //     })
 
-            axios.post('https://danielapi.herokuapp.com/public_html/api/user', {
+            const user = {
                 username: this.state.usuario_cadastro,
                 password: this.state.senha_cadastro
+            }
+
+            axios.post('https://danielapi.herokuapp.com/public_html/api/user', {user})
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
               })
-              .then(function (response) {
-                console.log(response);
-              })
-              .catch(function (error) {
-                console.error(error);
-              });
+            //   .then(function (response) {
+            //     console.log(response);
+            //   })
+            //   .catch(function (error) {
+            //     console.error(error);
+            //   });
 
             let elemento = document.getElementById("msgsucesso");
             elemento.className = "msgsucesso";
