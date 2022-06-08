5db9349c8cdfd741892e04ffed0d85ac2ea7e412
Geahn <contato@geahn.com>
2022-06-07 21:03:55 -0300

    vai

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-// import Login from "./Comps/Login";
-// import Cadastro from "./Comps/Cadastro";
 import Auth from "./Comps/Auth";
 import Listagem from "./Comps/Listagem";
 
